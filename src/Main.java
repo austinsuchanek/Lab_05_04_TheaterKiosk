09b556cda1927b826ff62f56405eae5bf03f9d3a
austinsuchanek <suchanad@mail.uc.edu>
2022-09-23 19:31:35 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age = 22;
+
+        if (age >= 21)
+        {
+            System.out.println("You are eligible to get a wristband! ");
+        }
+    }
+}
